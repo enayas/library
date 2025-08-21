aa4c3c7b7975290114fcdf91d8a0cda775bef4f0
Enaya Jawed <jawedenaya@gmail.com>
2025-08-20 19:46:45 -0500

    feat: create logic for storing book info in library array

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
-// const myLibrary = [book1, book2, book3];
+let myLibrary = [];
+
+// const book1 = new Book("Harry Potter and the Sorcerer's Stone", "J.K. Rowling", 309, true);
+// const book2 = new Book("The Poppy War", "R.F. Kuang", 544, false);
+// const book3 = new Book("Animal Farm", "George Orwell", 144, true);
 
 function Book(title, author, pages, readStatus){
   id = crypto.randomUUID;
   this.title = title;
   this.author = author;
   this.pages = pages;
-  this.read = read;
+  this.readStatus = readStatus;
 }
 
 
@@ -14,10 +18,6 @@ function addBookToLibrary(title, author, pages, readStatus){
   const book = new Book(title, author, Number(pages), readStatus);
   myLibrary.push(book);
 }
-
-// const book1 = new Book("Harry Potter and the Sorcerer's Stone", "J.K. Rowling", 309, true);
-// const book2 = new Book("The Poppy War", "R.F. Kuang", 544, false);
-// const book3 = new Book("Animal Farm", "George Orwell", 144, true);
 
 const modal = document.querySelector("#add-book-modal");
 
@@ -31,3 +31,23 @@ const exitButton = document.querySelector(".close-icon");
 exitButton.addEventListener("click", () => {
   modal.close();
 })
+
+const titleInput = document.getElementById("title");
+const authorInput = document.getElementById("author");
+const pagesInput = document.getElementById("pages");
+const readStatusInput = document.getElementById("read-status");
+
+let titleValue = "";
+let authorValue = "";
+let pagesValue = 0;
+let readStatusValue = "Unread";
+
+const submitButton = document.getElementById("add-button");
+submitButton.addEventListener("click", () => {
+  titleValue = titleInput.value;
+  authorValue = authorInput.value;
+  pagesValue = pagesInput.value;
+  readStatusValue = readStatusInput.value;
+
+  addBookToLibrary(titleValue, authorValue, pagesValue, readStatusValue);
+});
