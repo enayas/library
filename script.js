f423c783c898cd642b98dd76fa604e3ef9c84cbe
Enaya Jawed <jawedenaya@gmail.com>
2025-08-20 20:22:07 -0500

    feat: create logic for displaying book card in frontend; repetition bug fix needed

--- script.js
+++ script.js
@@ -12,11 +12,32 @@ function Book(title, author, pages, readStatus){
   this.readStatus = readStatus;
 }
 
-
-
 function addBookToLibrary(title, author, pages, readStatus){
   const book = new Book(title, author, Number(pages), readStatus);
   myLibrary.push(book);
+}
+
+function createBookCard(book){
+  const card = document.createElement("div");
+  const title = document.createElement("h4");
+  title.textContent = book.title;
+  const author = document.createElement("h5");
+  author.textContent = book.author;
+  const pages = document.createElement("h5");
+  pages.textContent = book.pages;
+  const readStatus = document.createElement("h5");
+  readStatus.textContent = book.readStatus;
+
+  card.append(title, author, pages, readStatus);
+
+  const cardContainer = document.getElementById("book-display");
+  cardContainer.append(card);
+}
+
+function updateCards(){
+  for(book of myLibrary){
+    createBookCard(book);
+  }
 }
 
 const modal = document.querySelector("#add-book-modal");
@@ -43,11 +64,13 @@ let pagesValue = 0;
 let readStatusValue = "Unread";
 
 const submitButton = document.getElementById("add-button");
-submitButton.addEventListener("click", () => {
+submitButton.addEventListener("click", (event) => {
+  event.preventDefault();
   titleValue = titleInput.value;
   authorValue = authorInput.value;
   pagesValue = pagesInput.value;
   readStatusValue = readStatusInput.value;
 
   addBookToLibrary(titleValue, authorValue, pagesValue, readStatusValue);
+  updateCards();
 });
